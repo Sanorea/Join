85c2632407c5a4492944931738b1d953b21bd067
Isabel Egli <isabel.egli@bluewin.ch>
2024-05-01 21:14:00 +0200

    addLogo

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 function docID(id) {
     return document.getElementById(id);
 }
+
+function renderHeader() {
+    docID('body').innerHTML=/*html*/`
+    <header>
+        <img src="./assets/img/logo_mobile.svg">
+        <div class="">G</div>
+    </header>`;
+}
+
+function renderNav() {
+    docID('body').innerHTML+=`<nav></nav>`;
+}
+
+function renderHeaderNav() {
+    renderHeader();
+    renderNav();
+}
+function renderContactSite() {
+    renderHeaderNav();
+    docID('body').innerHTML+=`<main id="main-contact"></main>`;    
+    docID('main-contact').innerHTML=``;
+}
+
+function renderIndexSite(){
+    renderHeaderNav();
+
+}
+
