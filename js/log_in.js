b3f4b9dd3c05623b2ff08a276b3e61e21de5971d
Eduard Kammerzel <likeaedu@gmail.com>
2024-05-04 01:14:56 +0200

    started with the log-in form

--- js/log_in.js
+++ js/log_in.js
@@ -1,10 +1,5 @@
 
 function init(){
-    loadLogo();
+    
 }
 
-function loadLogo(){
-const acoustic = document.querySelector('.acoustic');
-document.body.addEventListener('click', () => acoustic.classList.toggle('selected'));
-docID('mobile-logo-white').classList.add('dispaly-none');
-}
