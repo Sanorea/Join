5501e19106ee728093258049fd9d2febcac29de9
Eduard Kammerzel <likeaedu@gmail.com>
2024-05-06 21:35:18 +0200

    Log-in style is finish now started with first javascript (password input)

--- js/log_in.js
+++ js/log_in.js
@@ -4,3 +4,17 @@ function init(){
 }
 
 
+function showVisibility(){
+   
+        var x = document.getElementById("myInput");
+        if (x.type === "password") {
+          x.type = "text";
+        } else {
+          x.type = "password";
+        }
+      
+}
+
+function iconSwitch(){
+    
+}
