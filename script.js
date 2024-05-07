cdc3703fa001f30f15ba872da3aa23dfd7348256
Monomaze <brian@wester-mail.de>
2024-05-07 19:58:50 +0200

    added loadData() function to load data from Firebase backend

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const BASE_URL = "https://join-192-default-rtdb.europe-west1.firebasedatabase.app/";
+
 function docID(id) {
     return document.getElementById(id);
+}
+
+async function loadData() {
+    let response = await fetch(BASE_URL + ".json");
+    let responseToJson = await response.json();
+    console.log(responseToJson); 
 }
 
 function renderHeader() {
@@ -46,3 +54,4 @@ function renderHeaderNav() {
     renderHeader();
     renderNav();
 }
+
