d09661c6a2fecdf2ff0e624723c42d9fce2d9da2
Monomaze <brian@wester-mail.de>
2024-05-14 21:10:24 +0200

    deleted unused function

--- js/contacts.js
+++ js/contacts.js
@@ -169,23 +169,3 @@ function closeDialog(dialog) {
     dialogContainer.classList.remove('opened');
     dialogContainer.classList.add('closed');
 }
-
-/*
-function renderContactList() {
-    let contactList = docID('contact-list');
-    contactList.innerHTML = `
-    <div class="width-70 contacts-headline-letter-padding">
-        <div class="contacts-headline-letter">A</div>   
-    </div>
-    <div class="contacts-list-seperator width-80"></div>
-    <div class="contacts-contact-entry width-70 contacts-df-row-ai-center">
-        <div class="contacts-user-initials disp-flex-center-center">AM</div>
-        <div class="contacts-disp-flex-ai-start-fd-col contacts-contact"">
-            <p class="contacts-name">Anton Meyer</p>
-            <p class="contacts-email">anton@meyer.de</p>
-        </div>
-    </div>
-    <button id="show-contact-btn" onclick="openDialog('add-contact')" class="btn-primary btn-wo-icon btn-circle disp-flex-center-center"><img src="/assets/img/person_add.svg" alt=""></button>
-    `
-}
-*/
