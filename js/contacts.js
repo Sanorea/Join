d378cb5adabb22eb17bc7d7f2b3b290342955cb5
Monomaze <brian@wester-mail.de>
2024-05-14 21:03:46 +0200

    added rendering of the contact list

--- js/contacts.js
+++ js/contacts.js
@@ -1,8 +1,10 @@
 let arrContacts = [];
+let arrHeadlineLetters = [];
+let currentId = 0;
 
-function init() {
+async function init() {
     renderHeaderNav();
-    getContactsData();
+    await getContactsData();
     renderContactList();
 }
 
@@ -15,11 +17,67 @@ async function getContactsData() {
     return contactData;
 }
 
-
-function sortContacts() {
-      arrContacts.sort((a, b) => (a['first-letter'] > b['first-letter'] ? 1 : -1));
-      console.log(arrContacts);
+function getHeadlineLetters() {
+    arrHeadlineLetters = [];
+    for (i = 0; i < arrContacts.length; i++) {
+        let letter = getFirstLetter(arrContacts[i]['contact-name']);
+        if (arrHeadlineLetters.indexOf(letter) === -1) {
+            arrHeadlineLetters.push(letter);
+        }
+        arrHeadlineLetters.sort();
+    }
+    console.log(arrHeadlineLetters);
 }
+
+
+function renderContactList() {
+    let arrContactsInList = [];
+    let contactList = docID('contact-list');
+    getHeadlineLetters();
+    // for loop to go through every headline letter that exists
+    for (let i = 0; i < arrHeadlineLetters.length; i++) {
+        contactList.innerHTML += generateContactListHTML(arrHeadlineLetters[i]);
+        // for loop to check which contacts start with the current headline letter
+        for (let j = 0; j < arrContacts.length; j++) {
+            let firstLetter = getFirstLetter(arrContacts[j]['contact-name']);
+            if (arrHeadlineLetters[i].includes(firstLetter) == true) {
+                arrContactsInList.push(arrContacts[j]);
+            }
+        }
+        arrContactsInList.sort();
+        // for loop to generate HTML for every contact for current headline letter
+        for (let k = 0; k < arrContactsInList.length; k++) {
+            currentId = arrContactsInList[k]['contact-id'];
+            contactList.innerHTML += generateContactHTML(arrContactsInList[k], currentId);
+        }
+        arrContactsInList = [];
+    }
+    contactList.innerHTML += `<button id="show-contact-btn" onclick="openDialog('add-contact')" class="btn-primary btn-wo-icon btn-circle disp-flex-center-center"><img src="/assets/img/person_add.svg" alt=""></button>`
+}
+
+
+function generateContactHTML(contact, id) {
+    return `
+    <div id="contact${id}" class="contacts-contact-entry width-70 contacts-df-row-ai-center">
+        <div class="contacts-user-initials disp-flex-center-center">${contact['contact-acronym']}</div>
+        <div class="contacts-disp-flex-ai-start-fd-col contacts-contact"">
+            <p class="contacts-name">${contact['contact-name']}</p>
+            <p class="contacts-email">${contact['contact-email']}</p>
+        </div>
+    </div>
+    `
+}
+
+
+function generateContactListHTML(headlineLetter) {
+    return `
+    <div class="width-70 contacts-headline-letter-padding">
+        <div class="contacts-headline-letter">${headlineLetter}</div>   
+    </div>
+    <div class="contacts-list-seperator width-80"></div>
+    `
+}
+
 
 function saveContactDataInArray(contactData) {
     let tempArrContacts = [];
@@ -39,12 +97,13 @@ function saveContactDataInArray(contactData) {
 
 async function addContact() {
     let contactInputValues = setContactInputValues();
-    await postContactData(contactInputValues.name.value, contactInputValues.email.value, contactInputValues.phone.value, contactInputValues.firstLetterOfName, contactInputValues.acronym, contactInputValues.id);
+    await postContactData(contactInputValues.name.value, contactInputValues.email.value, contactInputValues.phone.value, contactInputValues.acronym, contactInputValues.id);
     emptyContactsInput(contactInputValues.name, contactInputValues.email, contactInputValues.phone);
     closeDialog('add-contact');
     getContactsData();
     renderContactList();
 }
+
 
 function setContactInputValues() {
     let name = docID('contact-name');
@@ -72,7 +131,7 @@ function setID() {
     if (arrContacts.length == 0) {
         id = 1;
     } else {
-        let lastUsedId = arrContacts[arrContacts.length - 1][1]['contact-id'];
+        let lastUsedId = arrContacts[arrContacts.length - 1]['contact-id'];
         id = lastUsedId + 1;
     }
 
@@ -87,13 +146,12 @@ function emptyContactsInput(name, email, phone) {
 }
 
 
-async function postContactData(name, email, phone, firstLetterOfName, acronym, id) {
+async function postContactData(name, email, phone, acronym, id) {
     await postData("/contacts", {
         "contact-name": name,
         "contact-email": email,
         "contact-tel": phone,
         "contact-acronym": acronym,
-        "first-letter": firstLetterOfName,
         "contact-id": id
     })
 }
@@ -112,7 +170,7 @@ function closeDialog(dialog) {
     dialogContainer.classList.add('closed');
 }
 
-
+/*
 function renderContactList() {
     let contactList = docID('contact-list');
     contactList.innerHTML = `
@@ -127,29 +185,7 @@ function renderContactList() {
             <p class="contacts-email">anton@meyer.de</p>
         </div>
     </div>
-
-    <div class="width-70 contacts-headline-letter-padding">
-        <div class="contacts-headline-letter">B</div>   
-    </div>
-    <div class="contacts-seperator width-80"></div>
-    <div class="contacts-contact-entry width-70 contacts-df-row-ai-center">
-        <div class="contacts-user-initials disp-flex-center-center">BS</div>
-        <div class="contacts-disp-flex-ai-start-fd-col contacts-contact"">
-            <p class="contacts-name">Beate Schulz</p>
-            <p class="contacts-email">beate@web.de</p>
-        </div>
-    </div>
     <button id="show-contact-btn" onclick="openDialog('add-contact')" class="btn-primary btn-wo-icon btn-circle disp-flex-center-center"><img src="/assets/img/person_add.svg" alt=""></button>
     `
 }
-
-
-/* -- might be used later -- */
-async function getHeadlineLetters() {
-    let contactData = await getContactsData();
-    
-    for(i = 0; i < contactData.length; i++) {
-        let firstLetter = contactData[i]['contact-name'].slice(0,1);
-        console.log(firstLetter);
-    }
-} 
+*/
