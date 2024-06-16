316e76c6470c66554bd2102759968981a85aa2a1
Eduard Kammerzel <likeaedu@gmail.com>
2024-06-16 13:23:22 +0200

    subtask is now working

--- js/add_task.js
+++ js/add_task.js
@@ -27,10 +27,10 @@ function renderList() {
 
 function renderSubtaskList(element,i) {
     return /*HTML*/`
-    <div class="list">
+    <div id="editId_${i}" class="list">
        <li>${element}</li>
        <div class="img-content">
-           <img src="/assets/img/edit_icon.svg" alt="Bearbeiten">
+           <img onclick="editSubtask(${i})" src="/assets/img/edit_icon.svg" alt="Bearbeiten">
            <div class="split"></div>
            <img onclick="deletTask(${i})" src="/assets/img/delete_icon.svg" alt="Löschen">
        </div>
@@ -46,6 +46,40 @@ function deleteValue() {
 function deletTask(element) {
     subtaskArray.splice(element, 1);
     renderList();
+}
+
+function editSubtask(i) { 
+    // let subtaskList = docID('add-task-subtasks-list');
+    let subtask = subtaskArray[i];
+    let editTask = docID(`editId_${i}`);
+    editTask.innerHTML = renderSwitchToInput(i);
+    let edit = docID(`edit_content_${i}`);
+    let hallo = docID(`newID_${i}`);
+    hallo.value = subtask;
+    docID('newInputSwitchImg').innerHTML = `<img onclick="deletTask(${i})" src="/assets/img/delete_icon.svg" alt=""></img>`;
+    docID('otherInputSitchImg').innerHTML = `<img onclick="finishEditInput(${i})" src="assets/img/check-task.svg" alt=""></img>`;
+    edit.classList.add('aktiveInput');
+}
+
+function renderSwitchToInput(i) {
+    return /*HTML*/ `
+    <div id="edit_content_${i}" class="list">
+        <li><input class="newEditInput" id="newID_${i}" type="text"></li>
+        <div class="img-content">
+        <div id="newInputSwitchImg"><img onclick="editSubtask(${i})" src="/assets/img/edit_icon.svg" alt="Bearbeiten"></div>
+           <div class="split"></div>
+           <div id="otherInputSitchImg"><img onclick="deletTask(${i})" src="/assets/img/delete_icon.svg" alt="Löschen"></div>
+           </div>
+    </div>
+    `;
+}
+
+function finishEditInput(i) {
+    let newElement = docID(`newID_${i}`).value;
+    let editTask = docID(`editId_${i}`);
+    subtaskArray[i] = newElement; 
+    let edit = subtaskArray[i];
+    editTask.innerHTML = renderSubtaskList(edit,i);
 }
 
                          
